f99f3d745eebb0438ecd0b7bb738f366a89a04a5
Nívea Almeida <niveaba.santos@gmail.com>
2026-07-30 22:38:34 -0300

    Add initial revenue calculation script

---
+++ 01-fundamentos/calculo-faturamento.py
@@ -0,0 +1,6 @@
+faturamento_inicial = int(50000)
+percentual_bonus = float(0.10)
+bonus = faturamento_inicial * percentual_bonus
+print(bonus)
+faturamento_total = faturamento_inicial - (bonus)
+print(faturamento_total)
