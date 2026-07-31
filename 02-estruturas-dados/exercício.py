ca1ef1a895c69fd00939725cbff607a7d1426eef
Nívea Almeida <niveaba.santos@gmail.com>
2026-07-30 21:38:05 -0300

    Criado usando o Colab

---
+++ 02-estruturas-dados/exercício.py
@@ -0,0 +1,58 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyM9U96ly4w7nnzmRxDD0Bp2",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/NiveaAlmeida/minha-jornada-python/blob/main/02-estruturas-dados/exerc%C3%ADcio.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "caMej-LS7dJ5",
+        "outputId": "149063a2-f520-46bc-f1ef-63f4644814a9"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "8\n"
+          ]
+        }
+      ],
+      "source": [
+        "def somar(a, b):\n",
+        "    resultado = a + b\n",
+        "    return resultado\n",
+        "\n",
+        "total = somar(5, 3)\n",
+        "print(total)  # 8"
+      ]
+    }
+  ]
+}
