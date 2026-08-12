b427faebc8b942e79c48b6a7add361aff894d59a
Nívea Almeida <niveaba.santos@gmail.com>
2026-08-12 11:35:19 -0300

    Implement list operations in exercicio-listas.py
    Add a list manipulation example with user input.

---
+++ 02-estruturas-dados/exercicio-listas.py
@@ -0,0 +1,7 @@
+chapeu = [1, 2, 3, 4, 5] 
+
+chapeu[2] = int(input('Digite um número: ')) #Substitue o número do meio por um digitado pelo usuário
+del chapeu[-1] #Deleta o último numero da lista
+print(len(chapeu)) #Imprime o comprimento da lisa
+print(chapeu) #Imprime a lista
+
